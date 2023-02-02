9e4a69dcd14a4efec734ad0e435ffcc983e16dd7
Nascimento Placido Belmiro Tobias <NascimeB@kapsch.net>
2023-02-02 17:07:36 -0300

    commit helloWord

---
+++ src/main/java/com/example/demo/HellowordbelmiroApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowordbelmiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowordbelmiroApplication.class, args);
+	}
+
+}
